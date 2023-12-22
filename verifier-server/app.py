7e2d1a1fece07a6b2a627e583fe651b26063e257
Sudipta Basak <sudiptab2100@gmail.com>
2023-12-22 15:09:37 +0530

    Added whatsapp messaging

--- verifier-server/app.py
+++ verifier-server/app.py
@@ -1,6 +1,8 @@
 import requests
 import json
-import threading, time
+import threading
+import time
+import pyautogui
 
 
 event_addr = "0x750e3394f4551dcf9d61b5152260ddf6c0cdf781064874bb27a66c330072d31d"
@@ -32,9 +34,26 @@ def get_pending_tasks(complete_tasks=False):
     
     return tasks
 
+def send_secret(phone, secret):
+    my_coord = [1059, 456]
+    message_box_coord = [1572, 1019]
+    send_button_coord = [1640, 1021]
+    link_coord = [1465, 948]
+
+    pyautogui.click(message_box_coord[0], message_box_coord[1])
+    time.sleep(1)
+    pyautogui.write(f"https://wa.me/{phone}?text={secret}")
+    pyautogui.click(send_button_coord[0], send_button_coord[1])
+    time.sleep(1)
+    pyautogui.click(link_coord[0], link_coord[1])
+    time.sleep(2)
+    pyautogui.click(send_button_coord[0], send_button_coord[1])
+    time.sleep(1)
+    pyautogui.click(my_coord[0], my_coord[1])
+
 def background():
     while True:
-        for task in get_pending_tasks(complete_tasks=True):
+        for task in get_pending_tasks(complete_tasks=False):
             print(task)
         print('\n\n')
         time.sleep(5)
